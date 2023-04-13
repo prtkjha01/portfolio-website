8a4fe0472684551fc79a7222f250ca3b05504a93
prateek-jha <prtkjha01@gmail.com>
2023-04-13 13:14:44 +0530

    fixed footer icons not showing error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,85 +1,76 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import './styles/Footer.css'
+import "./styles/Footer.css";
 const contacts = [
   {
     id: 1,
-    icon: 'fa-brands fa-github',
-    name: 'Github',
-    link: 'https://github.com/prtkjha01'
+    icon: <FontAwesomeIcon icon={"fa-brands fa-github"} />,
+    name: "Github",
+    link: "https://github.com/prtkjha01",
   },
   {
     id: 2,
-    icon: 'fa-solid fa-envelope',
-    name: 'Gmail',
-    link: 'https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=prtkjha01@gmail.com'
+    icon: <FontAwesomeIcon icon={"fa-solid fa-envelope"} />,
+    name: "Gmail",
+    link: "https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=prtkjha01@gmail.com",
   },
   {
     id: 3,
-    icon: 'fa-brands fa-linkedin',
-    name: 'LinkedIn',
-    link: 'https://www.linkedin.com/in/prateekjha01/'
+    icon: <FontAwesomeIcon icon={"fa-brands fa-linkedin"} />,
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/prateekjha01/",
   },
   {
     id: 4,
-    icon: 'fa-solid fa-code',
-    name: 'Leetcode',
-    link: 'https://leetcode.com/Prateek01/'
-  }
-]
+    icon: <FontAwesomeIcon icon={"fa-solid fa-code"} />,
+    name: "Leetcode",
+    link: "https://leetcode.com/Prateek01/",
+  },
+];
 const Footer = () => {
   return (
     <footer
-      id='contact'
-      className='footer p-4'
-      style={
-        {
-          backgroundColor: '#0D4C92',
-          color: '#fff',
-          position: 'relative',
-          left: 0,
-          right: 0,
-          bottom: 0
-        }
-      }
+      id="contact"
+      className="footer p-4"
+      style={{
+        backgroundColor: "#0D4C92",
+        color: "#fff",
+        position: "relative",
+        left: 0,
+        right: 0,
+        bottom: 0,
+      }}
     >
-      <div
-        className="contact-text container"
-        style={{ padding: '0 ' }}
-      >
+      <div className="contact-text container" style={{ padding: "0 " }}>
         <p>Contact Me</p>
         <br />
-        <div
-          className='row icon-row'
-        >
-          {
-            contacts.map((contact) => {
-              return (
-                <div className="contact-items col-lg-3 col-md-3 col-3">
-                  <div>
-                    <FontAwesomeIcon icon={contact.icon} />
-                  </div>
-                  <p className='Contact-item mx-2'>
-                    <a
-                      className='contactItem-text'
-                      href={contact.link}
-                      style={{ textDecoration: 'none', color: 'inherit' }}
-                      rel="noreferrer"
-                      target='_blank'
-                    >
-                      {contact.name}
-                    </a>
-                  </p>
+        <div className="row icon-row">
+          {contacts.map((contact) => {
+            return (
+              <div className="contact-items col-lg-3 col-md-3 col-3">
+                <div>
+                  {/* <FontAwesomeIcon icon={contact.icon} /> */}
+                  {contact.icon}
                 </div>
-              )
-            })
-          }
+                <p className="Contact-item mx-2">
+                  <a
+                    className="contactItem-text"
+                    href={contact.link}
+                    style={{ textDecoration: "none", color: "inherit" }}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    {contact.name}
+                  </a>
+                </p>
+              </div>
+            );
+          })}
         </div>
-
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
